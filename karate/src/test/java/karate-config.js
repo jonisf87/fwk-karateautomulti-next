377c91c66b39e17636efc4a2bd85328f5844541b
jonisf87 <84126445+jonisf87@users.noreply.github.com>
2025-07-17 13:58:21 +0200

    feat: configuración de entorno local, microservicio de ejemplo, integración swagger y test Karate funcionales

--- karate/src/test/java/karate-config.js
+++ karate/src/test/java/karate-config.js
@@ -1,23 +1,18 @@
 function fn() {
-  var env = karate.env; // get system property 'karate.env'
+  var env = karate.env;
   karate.log('karate.env system property was:', env);
   if (!env) {
     env = 'pre';
   }
-  var config = {
-    env: env,
-   
-    //myVarName: 'someValue'
-  }
-  
-  karate.set(read('classpath:config-'+env+'.yml'));
-  
-  if (env == 'mock') {
-    // customize
-    // e.g. config.foo = 'bar';
-    
-  } else if (env == 'des') {
-    // customize
-  }
+  var config = { env: env };
+
+  // Cargar configuración YAML según el entorno
+  var configFile = 'classpath:config-' + env + '.yml';
+  karate.log('Cargando configuración:', configFile);
+  var loadedConfig = karate.read(configFile);
+  karate.log('Configuración cargada:', loadedConfig);
+  karate.configure('headers', { 'Content-Type': 'application/json' });
+  karate.set(loadedConfig);
+
   return config;
 }
